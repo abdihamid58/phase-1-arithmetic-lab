af1d71a984eb01e1475f519177b416988ebee7d0
abdihamid <ibrahimabdy58@gmail.com>
2022-11-24 07:59:44 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,2 +1,12 @@
 
 // Write your code here
+let num1=31;
+let num2=2;
+let multiply=num1*num2
+let num3=20
+let num4=5
+let mod=num3/num4
+let max;
+max=Math.max(20,1)
+ let random;
+ random= (Math.floor(Math.random()*12)+2);
